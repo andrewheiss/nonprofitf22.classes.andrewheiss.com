d0dbb09cae574d1424e78f5f2d4a46db1b80490d
Andrew Heiss <andrew@andrewheiss.com>
2022-08-05 21:26:26 -0400

    Create schedule page and ical file

--- _targets.R
+++ _targets.R
@@ -2,8 +2,8 @@ library(targets)
 library(tarchetypes)
 suppressPackageStartupMessages(library(tidyverse))
 
-class_number <- "PMAP 8521"
-base_url <- "https://evalf22.classes.andrewheiss.com/"
+class_number <- "PMAP 3210"
+base_url <- "https://nonprofitf22.classes.andrewheiss.com/"
 page_suffix <- ".html"
 
 options(tidyverse.quiet = TRUE,
@@ -24,8 +24,19 @@ here_rel <- function(...) {fs::path_rel(here::here(...))}
 
 # Load functions for the pipeline
 # source("R/tar_slides.R")
+source("R/tar_calendar.R")
 
 # THE MAIN PIPELINE ----
 list(
+  ## Class schedule calendar ----
+  tar_target(schedule_file, here_rel("data", "schedule.csv"), format = "file"),
+  tar_target(schedule_page_data, build_schedule_for_page(schedule_file)),
+  tar_target(schedule_ical_data, build_ical(schedule_file, base_url, page_suffix, class_number)),
+  tar_target(schedule_ical_file,
+             save_ical(schedule_ical_data,
+                       here_rel("files", "schedule.ics")),
+             format = "file"),
 
+  ## Build site ----
+  tar_quarto(site, path = ".")
 )
