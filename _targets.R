ab9af99fa9fdab5155eed48132430ded4444716f
Andrew Heiss <andrew@andrewheiss.com>
2022-08-05 21:06:51 -0400

    Use targets

---
+++ _targets.R
@@ -0,0 +1,31 @@
+library(targets)
+library(tarchetypes)
+suppressPackageStartupMessages(library(tidyverse))
+
+class_number <- "PMAP 8521"
+base_url <- "https://evalf22.classes.andrewheiss.com/"
+page_suffix <- ".html"
+
+options(tidyverse.quiet = TRUE,
+        dplyr.summarise.inform = FALSE)
+
+tar_option_set(
+  packages = c("tibble"),
+  format = "rds",
+  workspace_on_error = TRUE
+)
+
+# here::here() returns an absolute path, which then gets stored in tar_meta and
+# becomes computer-specific (i.e. /Users/andrew/Research/blah/thing.Rmd).
+# There's no way to get a relative path directly out of here::here(), but
+# fs::path_rel() works fine with it (see
+# https://github.com/r-lib/here/issues/36#issuecomment-530894167)
+here_rel <- function(...) {fs::path_rel(here::here(...))}
+
+# Load functions for the pipeline
+# source("R/tar_slides.R")
+
+# THE MAIN PIPELINE ----
+list(
+
+)
